5c0e336fd0fc68d67863439041acfd39db8f25d8
Methni0616 <methnimanarandi7@gmail.com>
2026-02-09 15:50:38 +0530

    update

---
+++ portfolio-react/src/data/projects.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    title: "Role-Based Library System",
+    description: "Java MVC web application with authentication and MySQL",
+  },
+  {
+    title: "POS System",
+    description: "Supplier-based product and purchase order system",
+  },
+  {
+    title: "Internship Management System",
+    description: "Java EE system with filters and AJAX",
+  },
+];
+
+export default projects;
