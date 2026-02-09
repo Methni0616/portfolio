e1b1ee2929597ec55dcb1a399af9116f6df661c0
Methni0616 <methnimanarandi7@gmail.com>
2026-02-09 15:50:36 +0530

    update

---
+++ portfolio-react/src/components/Skills.jsx
@@ -0,0 +1,19 @@
+function Skills() {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <div className="skills-grid">
+        <span>Java</span>
+        <span>HTML</span>
+        <span>CSS</span>
+        <span>JavaScript</span>
+        <span>React</span>
+        <span>JSP / Servlets</span>
+        <span>MySQL</span>
+        <span>MVC Architecture</span>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
