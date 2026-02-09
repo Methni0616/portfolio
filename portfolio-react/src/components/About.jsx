375c7786900f6fa237f5d18a71bfdfd215cd0b44
Methni0616 <methnimanarandi7@gmail.com>
2026-02-09 15:50:15 +0530

    update

---
+++ portfolio-react/src/components/About.jsx
@@ -0,0 +1,14 @@
+function About() {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a software engineering undergraduate passionate about Java,
+        web development, MVC architecture, and cybersecurity. I enjoy
+        building real-world applications.
+      </p>
+    </section>
+  );
+}
+
+export default About;
