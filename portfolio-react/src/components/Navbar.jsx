1fc3b8afbc3ad9653513fc5d6c0e92330502ed7d
Methni0616 <methnimanarandi7@gmail.com>
2026-02-09 15:50:29 +0530

    update

---
+++ portfolio-react/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h3>Methni</h3>
+      <div>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
