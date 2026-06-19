3d2cf96d277618f99fa2936832ec237394a2ad04
Methni0616 <methnimanarandi7@gmail.com>
2026-06-19 15:24:11 +0530

    update

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -4,10 +4,12 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import ParticlesBackground from "./components/ParticlesBackground";
 
 function App() {
   return (
     <>
+      <ParticlesBackground />
       <Navbar />
       <Hero />
       <About />
