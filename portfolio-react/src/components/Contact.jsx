279382bc3e087546f9dc7b39947aa234b598ebd5
Methni0616 <methnimanarandi7@gmail.com>
2026-06-19 14:06:56 +0530

    update

--- portfolio-react/src/components/Contact.jsx
+++ portfolio-react/src/components/Contact.jsx
@@ -4,9 +4,34 @@ function Contact() {
       <h2 className="section-title">Contact</h2>
 
       <div className="contact-box">
-        <p><strong>Email:</strong> methnimanarandi7@gmail.com</p>
-        <p><strong>GitHub:</strong> github.com/Methni0616</p>
-        <p><strong>LinkedIn:</strong> linkedin.com/in/Methni-Manarandi</p>
+        <p>
+          <strong>Email:</strong>
+          <a href="mailto:methnimanarandi7@gmail.com">
+            methnimanarandi7@gmail.com
+          </a>
+        </p>
+
+        <p>
+          <strong>GitHub:</strong>
+          <a
+            href="https://github.com/Methni0616"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub Profile
+          </a>
+        </p>
+
+        <p>
+          <strong>LinkedIn:</strong>
+          <a
+            href="https://linkedin.com/in/methni-manarandi-196465270"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn Profile
+          </a>
+        </p>
       </div>
     </section>
   );
